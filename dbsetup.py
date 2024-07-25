10b3deb6f023baeb3471be3a832a949f06b29e9a
Tony Mason <fsgeek@cs.ubc.ca>
2024-07-25 11:16:10 -0700

    Fix a few more issues.  Add robustness in case Docker is not running.

--- dbsetup.py
+++ dbsetup.py
@@ -33,10 +33,12 @@ def run_container(db_config: IndalekoDBConfig):
         logging.critical('run_container: there is no "container", "volume" or "admin_password" configuration in the config file')
         exit(1)
 
-    indaleko_docker = IndalekoDocker(**{'container_name': db_config['container'], 'container_volume':db_config['volume']})
+    indaleko_docker = IndalekoDocker(**{'container_name': db_config['container'],
+                                        'container_volume':db_config['volume']})
 
     if db_config['container'] not in indaleko_docker.list_containers():
-        logging.debug(f'run_container: there is no container with the name "{db_config['container']}"! Creating one ...')
+        logging.debug('run_container: there is no container with the name "%s"!\
+                       Creating one ...', db_config['container'])
         # we don't have the container! create one
         indaleko_docker.create_container(
             container_name=db_config['container'],
@@ -113,7 +115,7 @@ def check_command(args : argparse.Namespace) -> None:
 
     # make sure the collections exist
     # must specify (call) db_config=db_config or script will not work
-    IndalekoCollections(db_config=db_config)
+    IndalekoCollections(db_config=db_config=db_config)
 
 
 def delete_command(args : argparse.Namespace) -> None:
