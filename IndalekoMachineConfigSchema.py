f9a0c661a6160f48eef53e19e6382c5fe7b6baed
Tony Mason <tony@wamason.com>
2024-08-09 09:08:00 -0700

    Use correct schema with MachineConfig.

--- IndalekoMachineConfigSchema.py
+++ IndalekoMachineConfigSchema.py
@@ -34,6 +34,10 @@ class IndalekoMachineConfigSchema(IndalekoRecordSchema):
         machine_config_rules = apischema.json_schema.deserialization_schema(
             IndalekoMachineConfigDataModel.MachineConfig,
             additional_properties=True)
+        if not hasattr(self, 'rules'):
+            self.rules = machine_config_rules
+        else:
+            self.rules.update(machine_config_rules)
         schema_id = 'https://activitycontext.work/schema/machineconfig.json'
         schema_title = 'Machine Configuration Schema'
         schema_description = 'Describes the machine where the data was indexed.'
