9b6c0384e8b0002dea53d83636c9a5ad188a1f45
Tony Mason <fsgeek@cs.ubc.ca>
2024-09-21 15:43:20 -0700

    Added skeleton for WiFi and Tile location providers.

---
+++ activity/providers/location/data_models/wifi_location_data_model.py
@@ -0,0 +1,55 @@
+'''
+This module defines the data model for the WiFibased location
+activity data provider.
+
+Project Indaleko
+Copyright (C) 2024 Tony Mason
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+import os
+import sys
+
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
+from icecream import ic
+
+if os.environ.get('INDALEKO_ROOT') is None:
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    while not os.path.exists(os.path.join(current_path, 'Indaleko.py')):
+        current_path = os.path.dirname(current_path)
+    os.environ['INDALEKO_ROOT'] = current_path
+    sys.path.append(current_path)
+
+from activity.providers.location.data_models.location_data_model import BaseLocationDataModel
+
+class WiFiLocationDataModel(BaseLocationDataModel):
+    '''
+    This class defines the data model for the WiFi-based location activity data provider.
+    '''
+    class Config(BaseLocationDataModel.Config):
+        '''
+        This class defines the configuration for the WiFi-based location activity data provider.
+        '''
+
+    def serialize(self):
+        '''Serialize the data model'''
+        return self.dict()
+
+    @staticmethod
+    def deserialize(data):
+        '''Deserialize the data model'''
+        return WiFiLocationDataModel(**data)
