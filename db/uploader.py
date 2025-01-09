8ac0920474fca9c5d7d6d9f35bbee88695c54b82
Tony Mason <fsgeek@cs.ubc.ca>
2025-01-09 09:58:25 -0800

    Initial skeletal uploader files created.

---
+++ db/uploader.py
@@ -0,0 +1,43 @@
+'''
+This is a class for supporting builk upload to the database.
+
+Project Indaleko
+Copyright (C) 2024 Tony Mason
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+import os
+import sys
+
+from icecream import ic
+
+if os.environ.get('INDALEKO_ROOT') is None:
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    while not os.path.exists(os.path.join(current_path, 'Indaleko.py')):
+        current_path = os.path.dirname(current_path)
+    os.environ['INDALEKO_ROOT'] = current_path
+    sys.path.append(current_path)
+
+# pylint: disable=wrong-import-position
+# pylint: enable=wrong-import-position
+
+class IndalekoDBUploader:
+    '''This class can be used when creating uploader(s) for the database.'''
+
+def main():
+    '''This is the main function for the database uploader.'''
+    ic('Testing the database uploader.')
+
+if __name__ == '__main__':
+    main()
